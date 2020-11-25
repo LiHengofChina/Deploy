9d63b083fd503d09aee1672aed80781c6e5f5d77
liheng <13688007165@139.com>
2020-11-25 23:36:21 +0800

    maven补充。

---
+++ maven/002.maven插件/003._____________________maven-compiler-plugin插件/maven-plugin-test-003/src/main/java/com/liheng/test/Demo.java
@@ -0,0 +1,21 @@
+package com.liheng.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Demo {
+	public static void main(String[] args) {
+		
+		List<String> list = new ArrayList<String>();
+		
+		list.add("a");
+		list.add("b");
+		list.add("c");
+		list.add("d");
+		list.add("e");
+		list.add("f");
+		
+		list.stream().forEach( System.out::println); 
+		
+	}
+}
